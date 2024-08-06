b3543b758df443c6e543e13123bf1b8e61df3ea4
sushanth1234reddy <svreddy6889@gmail.com>
2024-08-06 23:52:19 +0530

    1

---
+++ prog1.js
@@ -0,0 +1,11 @@
+//prog 1
+// onetime view msg
+// if the msg is onetimeview  then we can't forward it to another frnds
+//if not , we can forward to amother frnd
+let OneTimeMsg=true;
+if(OneTimeMsg){
+    console.log("can't forward it to another frnds");
+}
+else{
+    console.log("can forward it to another frnds");
+}
