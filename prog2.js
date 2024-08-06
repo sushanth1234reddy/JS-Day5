e3c1b43b539833d08660d209add7772a8894cf08
sushanth1234reddy <svreddy6889@gmail.com>
2024-08-06 23:52:44 +0530

    2

---
+++ prog2.js
@@ -0,0 +1,11 @@
+//prog 2
+//msg delete option
+//if time is <24hrs then show delete for me and delete for everyone
+//if time is >24hrs then show only delete for me option
+let Time=18;
+if(Time<24){
+    console.log(" show delete for me & delete for everyone ");
+}
+else{
+    console.log("show delete for me");
+}
